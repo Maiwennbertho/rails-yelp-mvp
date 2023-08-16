16f5654b3802aca584ff9b489d90d60d5edb8f3b
Maiwenn BERTHO <maiwenn.berth@gmail.com>
2023-08-16 15:04:44 +0200

    Ex 2 rails yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    member do
+      get 'restaurants/:id/reviews/new', to: 'reviews#new', as: :new_review
+      post 'restaurants/:id/reviews', to: 'reviews#create'
+    end
+    resources :reviews, only: %i[new create]
+  end
+  root to: 'restaurants#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destroy_all
+
+junior_resto = { name: 'Junior resto', address: '10 rue de la paix', phone_number: '333-450', category: 'chinese' };
+les_pizza_de_la_mama ={ name: 'Les pizzas de la mama', address: '15 rue di dolce', phone_number: '555-5678', category: 'italian' };
+arikuu = { name: 'Arikuu', address: '18 avenue de Naruto', phone_number: '555-9876', category: 'japanese' };
+le_nôtre = { name: 'Le nôtre', address: '72 route du château', phone_number: '555-5432', category: 'french' };
+fricadas = { name: 'Fricadas', address: '12 rue de la frite', phone_number: '555-8765', category: 'belgian' };
+
+[junior_resto, les_pizza_de_la_mama, arikuu, le_nôtre, fricadas].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
